aeaed5c747983da00644c0c3b12c9ff7fb24e0e4
Ashraful Dowla <ashrafuldowlaunited532@gmail.com>
2023-02-03 11:04:05 +0600

    dashboard dynamic

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Review;
+use App\Models\Ticket;
 use App\Models\User;
 use Auth;
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -18,7 +25,43 @@ class AdminController extends Controller
     //admin dashboard
     public function admin()
     {
-        return view('admin.home');
+        $customers = User::where('is_admin', '!=', 1)
+            ->orWhere('is_admin', null)
+            ->orderBy('id', 'desc')
+            ->limit(8)
+            ->get();
+
+        $orders = Order::orderBy('id', 'desc')
+            ->limit(8)
+            ->get();
+
+        $products = Product::where('product_status', 1)
+            ->orderBy('product_views', 'desc')
+            ->limit(8)
+            ->get();
+
+        $total_product = Product::count();
+        $active_product = Product::where('product_status', 1)->count();
+        $inactive_product = Product::where('product_status', 0)->count();
+
+        $all_customers = User::where('is_admin', '!=', 1)
+            ->orWhere('is_admin', null)
+            ->count();
+
+        $category = Category::count();
+        $brand = Brand::count();
+        $ticket = Ticket::where('status', 0)->count();
+        $review = Review::count();
+
+        $coupon = DB::table('coupons')->count();
+        $subscriber = DB::table('newsletters')->count();
+        $pending_order = Order::where('status', 0)->count();
+        $success_order = Order::where('status', 3)->count();
+
+        // return response()->json($total_products);
+        return view('admin.home', compact('customers', 'orders', 'products', 'total_product', 'active_product',
+            'inactive_product', 'all_customers', 'category', 'brand', 'ticket', 'review',
+            'coupon', 'subscriber', 'pending_order', 'success_order'));
     }
 
     //admin password change
